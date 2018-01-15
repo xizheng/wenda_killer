78993d5d4c9c5866daae6b0f83b4c5e5989fcfb2
wenbo.zhang <wenbo.zhang@duitang.com>
2018-01-15 10:23:39 +0800

    debug

--- server/demo/sse-server.js
+++ server/demo/sse-server.js
@@ -16,12 +16,17 @@ app.get('/sse', (req, res) => {
   var id_cddh = ''
   var id_zscr = ''
   var result11 = ''
-  request.cookie('vrpos=R71mqrGbpODh9V6n8HOJb70y2V4Trj8JQWD9Gvxm9MU=; dt_ssuid=6277428700; usid=ex-hDNi__ymfhoZU; FREQUENCY=1515816551863_1; ld=plllllllll2zQYBulllllVIBPuYlllllNn3uqkllll9lllllRylll5@@@@@@@@@@; wuid=AAEm8FKkHQAAAAqROm/rvgsAZAM=; SNUID=31CE8513686205C8EC652ECE681749AB; SUID=56A6E2742513910A000000005A598666; IPLOC=CN3100; SUV=00F52C4874E2812D5A584227E861B755')
+  var j = request.jar();
+  var cookie = request.cookie('vrpos=R71mqrGbpODh9V6n8HOJb70y2V4Trj8JQWD9Gvxm9MU=; dt_ssuid=6277428700; usid=ex-hDNi__ymfhoZU; FREQUENCY=1515816551863_1; ld=plllllllll2zQYBulllllVIBPuYlllllNn3uqkllll9lllllRylll5@@@@@@@@@@; wuid=AAEm8FKkHQAAAAqROm/rvgsAZAM=; SNUID=31CE8513686205C8EC652ECE681749AB; SUID=56A6E2742513910A000000005A598666; IPLOC=CN3100; SUV=00F52C4874E2812D5A584227E861B755')
+  var url = 'http://wd.sa.sogou.com';
+  j.setCookie(cookie, url);
   
   const pusher = setInterval(() => {
 
     var options_xigua = {
     url: 'http://wd.sa.sogou.com/api/ans?key=xigua',
+    jar: true,
+    jar: j,
     headers: {
       'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_2_2 like Mac OS X) AppleWebKit/604.4.7 (KHTML, like Gecko) Mobile/15C202 Sogousearch/Ios/5.9.7',
       'referer': 'http://wd.sa.sogou.com/'
@@ -29,40 +34,39 @@ app.get('/sse', (req, res) => {
     };
     function callback_xigua(error, response, body) {
       console.log(body)
-      console.log(response)
       if (!error && response.statusCode == 200) {
         var info1 = JSON.parse(body)
-        var result1 = JSON.parse(info1.result[1])
-        if (result1.result=="啊呀，这题汪仔还在想"){
-          result1.result = "这题我不会，靠你了"
-        }
-        if (id_xigua=='' || id_xigua!=result1.cd_id){
-           sseStream.write({
-             event: 'xigua',
-             data: result1.title+'\n【AI答案】:'+'【'+result1.result+'】'
-           })
-           id_xigua = result1.cd_id
-        }
+        // var result1 = JSON.parse(info1.result[1])
+        // if (result1.result=="啊呀，这题汪仔还在想"){
+        //   result1.result = "这题我不会，靠你了"
+        // }
+        // if (id_xigua=='' || id_xigua!=result1.cd_id){
+        //    sseStream.write({
+        //      event: 'xigua',
+        //      data: result1.title+'\n【AI答案】:'+'【'+result1.result+'】'
+        //    })
+        //    id_xigua = result1.cd_id
+        // }
       }
     }
     request(options_xigua, callback_xigua);
 
-    request('http://crop-answer.sm.cn/answer/curr?format=json&activity=million', function (error1, response1, body1) {
-         if (!error1 && response1.statusCode == 200) {
-           var info11 = JSON.parse(body1)
-           if (info11.data.round){
-             result11 = info11.data.options[parseInt(info11.data.correct)].title
-           }
-           if (info11.data.round && uc_xigua!=info11.data.round){
-           sseStream.write({
-             event: 'xigua',
-             data: info11.data.round+'. '+info11.data.title+'【枪手答案】:【'+result11+'】'
-           })
-           uc_xigua = info11.data.round
-           }
+    // request('http://crop-answer.sm.cn/answer/curr?format=json&activity=million', function (error1, response1, body1) {
+    //      if (!error1 && response1.statusCode == 200) {
+    //        var info11 = JSON.parse(body1)
+    //        if (info11.data.round){
+    //          result11 = info11.data.options[parseInt(info11.data.correct)].title
+    //        }
+    //        if (info11.data.round && uc_xigua!=info11.data.round){
+    //        sseStream.write({
+    //          event: 'xigua',
+    //          data: info11.data.round+'. '+info11.data.title+'【枪手答案】:【'+result11+'】'
+    //        })
+    //        uc_xigua = info11.data.round
+    //        }
 
-         }
-        }) 
+    //      }
+    //     }) 
   //   var options_huajiao = {
   //   url: 'http://wd.sa.sogou.com/api/ans?key=huajiao',
   //   headers: {
